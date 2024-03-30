1bc7cbbb2b27c96c5e84c89ef56ad677cb3821df
GhizlaneAmjoun <159482724+GhizlaneAmjoun@users.noreply.github.com>
2024-03-29 23:17:16 -0400

    Add files via upload

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,32 @@
 package application;
 
-public class Main {
+import userInterface.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+
+public class Main extends Application {
+	
+	@Override
+	public void start (Stage primaryStage) {
+		
+		
+		UserInteraction userInteraction = new UserInteraction(primaryStage);
+		userInteraction.setButtonActions();
+
+		
+		
+		primaryStage.setTitle("TrafficWatch");	
+		primaryStage.setScene(userInteraction.createLoginScene());
+	
+		primaryStage.show();	
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
 }
